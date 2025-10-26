6cd32f2db70f89d6059c7e81314afd567c5e3adf
primabox <xemiro@proton.me>
2025-10-26 22:17:26 +0100

    details

--- js/curriculum.js
+++ js/curriculum.js
@@ -10,20 +10,10 @@
         // Toggle open class for smooth animation
         if (content.classList.contains('open')) {
           content.classList.remove('open');
-          toggle.classList.remove('fa-chevron-down');
-          toggle.classList.add('fa-chevron-right');
           toggle.style.transform = 'rotate(0deg)';
         } else {
           content.classList.add('open');
-          toggle.classList.remove('fa-chevron-right');
-          toggle.classList.add('fa-chevron-down');
-          toggle.style.transform = 'rotate(0deg)';
+          toggle.style.transform = 'rotate(180deg)';
         }
       });
     });
-    
-    // Initialize first module as open
-    const firstModule = document.querySelector('.curriculum-module .module-content');
-    if (firstModule) {
-      firstModule.classList.add('open');
-    }
